0fc7f1dc1e9c1426e2ff54be70d71a89bd7bcdaf
Nanda Rizki Nasution <nandarizki.nasution@gmail.com>
2022-08-28 03:06:18 +0700

    20220828 exploring debugger Breakpoint testing variable

--- mysite/hello/urls.py
+++ mysite/hello/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from hello import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("hello/<name>", views.hello_there, name="hello_there")
 ]
--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -1,7 +1,24 @@
+import re
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils.timezone import datetime
 
 # Create your views here.
-from django.http import HttpResponse
-
 def home(request):
     return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there," + clean_name + "! It's " + formatted_now
+    return HttpResponse(content)
